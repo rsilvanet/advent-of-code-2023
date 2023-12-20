5da958687a83eda5b6a9ee0b90c9df76101bef81
Robson <sc.robson@gmail.com>
2023-12-20 17:21:34 +0100

    Day 18 ✅

--- AdventOfCode/Day18/Day18.cs
+++ AdventOfCode/Day18/Day18.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Globalization;
+﻿using System.Globalization;
 
 internal static class Day18
 {
@@ -16,97 +15,59 @@ internal static class Day18
             return (direction, steps, hexa);
         }).ToArray();
 
-        var stopwatch = Stopwatch.StartNew();
+        var vertices = ParseVertices(commands).ToArray();
+        var verticesFromHexa = ParseVerticesFromHexa(commands).ToArray();
 
-        var loop = ParseLoop(commands).OrderBy(x => x.Start.column).ToArray();
-        Console.WriteLine($"Day 18, Part 1: parsed in {stopwatch.ElapsedMilliseconds}ms");
-        Console.WriteLine($"Day 18, Part 1: {Count(loop)} in {stopwatch.ElapsedMilliseconds}ms");
+        Console.WriteLine($"Day 18, Part 1: {CalculateArea(vertices)}");
+        Console.WriteLine($"Day 18, Part 2: {CalculateArea(verticesFromHexa)}");
 
-        stopwatch.Restart();
-
-        var loopFromHexa = ParseLoopFromHexa(commands).OrderBy(x => x.Start.column).ToArray();
-        Console.WriteLine($"Day 18, Part 2: parsed in {stopwatch.ElapsedMilliseconds}ms");
-        Console.WriteLine($"Day 18, Part 2: {Count(loopFromHexa)} in {stopwatch.ElapsedMilliseconds}ms");
-
-        long Count(Line[] loop)
+        long CalculateArea((int row, int column)[] vertices)
         {
-            var rowMin = loop.Min(x => x.RowMin);
-            var rowMax = loop.Max(x => x.RowMax);
-            var counter = loop.Sum(x => x.CountBlocks());
-            var verticalLines = loop.Where(x => x.IsVertical).ToArray();
-            var boundaries = loop.SelectMany(x => x.Points).ToHashSet();
-
-            for (int row = rowMin; row <= rowMax; row++)
+            long area = 0;
+            long contour = 0;
+            
+            for (int i = 0; i < vertices.Length; i++)
             {
-                var columnMin = loop.Where(x => x.Rows.Contains(row)).Min(x => x.ColumnMin);
-                var columnMax = loop.Where(x => x.Rows.Contains(row)).Max(x => x.ColumnMax);
-
-                for (int column = columnMin; column <= columnMax; column++)
-                {
-                    if (boundaries.Contains((row, column)))
-                    {
-                        continue;
-                    }
-
-                    var nextLineToTheRight = verticalLines.Where(x => x.ColumnMin > column && x.RowIsInRange(row)).FirstOrDefault();
-
-                    if (nextLineToTheRight == null)
-                    {
-                        continue;
-                    }
-
-                    var linesToTheLeft = 0;
-                    var lastLineDirection = 'X';
-                    var possibleLinesToTheLeft = verticalLines.Where(x => x.ColumnMin < column && x.RowIsInRange(row)).ToArray();
-
-                    foreach (var possibleLine in possibleLinesToTheLeft)
-                    {
-                        if (possibleLine.Direction != lastLineDirection)
-                        {
-                            linesToTheLeft++;
-                        }
-
-                        lastLineDirection = possibleLine.Direction;
-                    }
-
-                    if (linesToTheLeft % 2 == 1)
-                    {
-                        counter += nextLineToTheRight.Start.column - column;
-                    }
-
-                    column = nextLineToTheRight.Start.column;
-                }
+                int next = (i + 1) % vertices.Length;
+                area += (long)vertices[i].row * vertices[next].column;
+                area -= (long)vertices[i].column * vertices[next].row;
+                contour += CalculateDistance(vertices[i], vertices[next]);
             }
 
-            return counter;
+            area = Math.Abs(area) / 2;
+            area += contour / 2 + 1;
+            return area;
         }
 
-        IEnumerable<Line> ParseLoop((char direction, int steps, string hexa)[] commands)
+        long CalculateDistance((int row, int column) point1, (int row, int column) point2)
         {
-            var start = (row: 10_000_000, column: 10_000_000);
+            return (long)Math.Sqrt(Math.Pow(point2.row - point1.row, 2) + Math.Pow(point2.column - point1.column, 2));
+        }
+
+        IEnumerable<(int row, int column)> ParseVertices((char direction, int steps, string hexa)[] commands)
+        {
+            var current = (row: 0, column: 0);
 
             for (int i = 0; i < commands.Length; i++)
             {
                 var command = commands[i];
 
-                var end = command.direction switch
+                current = command.direction switch
                 {
-                    'D' => (start.row + command.steps, start.column),
-                    'U' => (start.row - command.steps, start.column),
-                    'R' => (start.row, start.column + command.steps),
-                    'L' => (start.row, start.column - command.steps),
+                    'D' => (current.row + command.steps, current.column),
+                    'U' => (current.row - command.steps, current.column),
+                    'R' => (current.row, current.column + command.steps),
+                    'L' => (current.row, current.column - command.steps),
                     _ => throw new NotImplementedException()
                 };
 
-                yield return new Line(start, end, command.direction);
-
-                start = end;
+                yield return current;
             }
         }
 
-        IEnumerable<Line> ParseLoopFromHexa((char direction, int steps, string hexa)[] commands)
+        IEnumerable<(int row, int column)> ParseVerticesFromHexa((char direction, int steps, string hexa)[] commands)
         {
-            var start = (row: 10_000_000, column: 10_000_000);
+            var current = (row: 0, column: 0);
 
             for (int i = 0; i < commands.Length; i++)
             {
@@ -122,63 +83,17 @@ internal static class Day18
                     _ => throw new NotImplementedException(),
                 };
 
-                var end = hexaDirection switch
+                current = hexaDirection switch
                 {
-                    'D' => (start.row + hexaSteps, start.column),
-                    'U' => (start.row - hexaSteps, start.column),
-                    'R' => (start.row, start.column + hexaSteps),
-                    'L' => (start.row, start.column - hexaSteps),
+                    'D' => (current.row + hexaSteps, current.column),
+                    'U' => (current.row - hexaSteps, current.column),
+                    'R' => (current.row, current.column + hexaSteps),
+                    'L' => (current.row, current.column - hexaSteps),
                     _ => throw new NotImplementedException()
                 };
 
-                yield return new Line(start, end, hexaDirection);
-
-                start = end;
+                yield return current;
             }
         }
-    }
-
-    class Line
-    {
-        public Line((int row, int column) start, (int row, int column) end, char direction)
-        {
-            Start = start;
-            End = end;
-            Direction = direction;
-            IsVertical = direction == 'U' || direction == 'D';
-            RowMin = Math.Min(Start.row, End.row);
-            RowMax = Math.Max(Start.row, End.row);
-            ColumnMin = Math.Min(Start.column, End.column);
-            ColumnMax = Math.Max(Start.column, End.column);
-            Rows = Enumerable.Range(RowMin, RowMax - RowMin + 1).ToHashSet();
-
-            if (RowMin == RowMax)
-            {
-                Points = Enumerable.Range(ColumnMin, ColumnMax - ColumnMin + 1).Select(x => (RowMin, x)).ToHashSet();
-            }
-            else
-            {
-                Points = Rows.Select(x => (x, ColumnMin)).ToHashSet();
-            }
-        }
-
-        public (int row, int column) Start { get; }
-        public (int row, int column) End { get; }
-        public char Direction { get; }
-        public bool IsVertical { get; }
-        public int RowMin { get; }
-        public int RowMax { get; }
-        public int ColumnMin { get; }
-        public int ColumnMax { get; }
-        public HashSet<int> Rows { get; }
-        public HashSet<(int row, int column)> Points { get; }
-
-        public long CountBlocks() => Points.Count - 1;
-
-        public bool ContainsBlock(int row, int column) => Points.Contains((row, column));
-
-        public bool RowIsInRange(int row) => Rows.Contains(row);
-
-        public override string ToString() => $"From {Start.row},{Start.column} to {End.row},{End.column} going {Direction}";
     }
 }
